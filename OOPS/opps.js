0f002c52bc78222d3c7dfc31c80fff417ed4d9a3
Priyanshu Panwar <priyanshugurjar066@gmail.com>
2024-11-22 10:57:31 +0530

    Add files via upload

---
+++ OOPS/opps.js
@@ -0,0 +1,37 @@
+const user = {
+    userName : "Priyanshu",
+    email : "priyanshu@gmail.com",
+    age : 25,
+    signedIn : true  
+,
+    getUserDetails: function(){
+        // console.log("Got user details from Database");
+        // console.log(`userName:${this.userName}`);
+        console.log(this);
+        
+    }
+}
+
+const user2 = {
+    userName : "Priyu",
+    email : "priyu@gmail.com",
+    age : 21,
+    signedIn : true  
+,
+    getUserDetails: function(){
+        // console.log("Got user details from Database");
+        // console.log(`userName:${this.userName}`);
+        console.log(this);
+        
+    }
+}
+
+
+
+
+// console.log(user.userName);
+// console.log(user.getUserDetails());
+// // console.log(this);
+// console.log(user2.userName);
+// console.log(user2.getUserDetails());
+
